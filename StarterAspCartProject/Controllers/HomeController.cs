0069babdbdc009c8b5f98a9d22e42646bffe27a2
mustbebuilt <martin@mustbebuilt.co.uk>
2021-03-23 09:20:00 +0000

    Set up

--- StarterAspCartProject/Controllers/HomeController.cs
+++ StarterAspCartProject/Controllers/HomeController.cs
@@ -15,7 +15,6 @@ namespace StarterAspCartProject.Controllers
     public class HomeController : Controller
     {
 
-        const string SessionCart = "_Cart";
         private readonly ILogger<HomeController> _logger;
 
         private readonly ApplicationDbContext _context;
@@ -79,85 +78,7 @@ namespace StarterAspCartProject.Controllers
             return View(model);
         }
 
-        [HttpPost]
-        public IActionResult FilmDetails(IFormCollection form)
-        {
-            int FilmID = int.Parse(form["FilmID"]);
-            string FilmTitle = form["FilmTitle"].ToString();
-            decimal FilmPrice = Decimal.Parse(form["FilmPrice"]);
-            int OrderQuantity = int.Parse(form["OrderQuantity"]);
-            CartItem newOrder = new CartItem();
-            newOrder.FilmID = FilmID;
-            newOrder.FilmTitle = FilmTitle;
-            newOrder.FilmPrice = FilmPrice;
-            newOrder.OrderQuantity = OrderQuantity;
-            newOrder.OrderDate = DateTime.Now;
-            var CartList = new List<CartItem>();
-            if (HttpContext.Session.GetString(SessionCart) != null)
-            {
-                string serialJSON = HttpContext.Session.GetString(SessionCart);
-                CartList = JsonSerializer.Deserialize<List<CartItem>>(serialJSON);
-                //
-                var item = CartList.FirstOrDefault(o => o.FilmID == FilmID);
-                if (item != null)
-                {
-                    item.OrderQuantity += OrderQuantity;
-                }
-                else
-                {
-                    CartList.Add(newOrder);
-                }
 
-            }
-            else
-            {
-                CartList.Add(newOrder);
-            }
-            HttpContext.Session.SetString(SessionCart, JsonSerializer.Serialize(CartList));
-            return RedirectToAction("FilmDetails");
-        }
-
-
-
-        [HttpGet]
-        public IActionResult ManageCart()
-        {
-            List<CartItem> cart = new List<CartItem>();
-            if (HttpContext.Session.GetString(SessionCart) != null)
-            {
-                string serialJSON = HttpContext.Session.GetString(SessionCart);
-                cart = JsonSerializer.Deserialize<List<CartItem>>(serialJSON);
-            }
-            if (TempData["msg"] != null)
-            {
-                ViewBag.msg = TempData["msg"].ToString();
-            }
-            return View(cart);
-        }
-
-        [HttpPost]
-        public IActionResult RemoveCartItem(IFormCollection form)
-        {
-            int FilmID = int.Parse(form["FilmID"]);
-            var CartList = new List<CartItem>();
-            if (HttpContext.Session.GetString(SessionCart) != null)
-            {
-                string serialJSON = HttpContext.Session.GetString(SessionCart);
-                CartList = JsonSerializer.Deserialize<List<CartItem>>(serialJSON);
-                //
-                var item = CartList.FirstOrDefault(o => o.FilmID == FilmID);
-                if (item != null)
-                {
-                    CartList.Remove(item);
-                }
-
-            }
-
-            HttpContext.Session.SetString(SessionCart, JsonSerializer.Serialize(CartList));
-            TempData["msg"] = "Item Removed";
-            return RedirectToAction("ManageCart");
-            //return RedirectToAction("ManageCart", new { id = FilmID });
-        }
 
 
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
